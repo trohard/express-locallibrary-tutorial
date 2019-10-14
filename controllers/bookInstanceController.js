a07072f06fb99adaa30e771450c28a9b2c37030b
Harding <tdh@salina.k-state.edu>
2019-10-14 10:41:41 -0500

    implement delete options

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -96,12 +96,37 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_delete', { title: 'Delete Book Instance', bookinstance:  bookinstance});
+    })
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+    BookInstance.findById(req.params.id)
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so delete.
+      BookInstance.findByIdAndRemove(req.body.bookinstanceid, function deleteBookinstance(err) {
+        if (err) { return next(err); }
+        // Success - go to author list
+        res.redirect('/catalog/bookinstances')
+      })
+    })
 };
 
 // Display BookInstance update form on GET.
